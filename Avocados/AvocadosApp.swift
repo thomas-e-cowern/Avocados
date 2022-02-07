050fbc2f0562e493e835555b7b74398afcac2fe4
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-07 08:41:50 -0500

    initial commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Thomas Cowern New on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
