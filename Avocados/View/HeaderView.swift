6249fecbd5ea07b2e2b25b3213286429cd0f4673
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-09 10:37:35 -0500

    added images and text to header view

--- Avocados/View/HeaderView.swift
+++ Avocados/View/HeaderView.swift
@@ -13,15 +13,33 @@ struct HeaderView: View {
             Image("avocado-slice-1")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
-            VStack {
-                Text("Avacodo Header")
+            
+            HStack (alignment: .top, spacing: 0) {
+                Rectangle()
+                    .fill(Color("ColorGreenLight"))
+                    .frame(width: 4)
                 
-                Text("Avocados are a powerhouse ingredient at any meal fo anyone")
+                VStack (alignment: .leading, spacing: 6) {
+                    Text("Avocado")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .shadow(radius: 3)
+                    
+                    Text("Avocados are a powerhouse ingredient at any meal fo anyone")
+                        .font(.footnote)
+                        .lineLimit(2)
+                        .multilineTextAlignment(.leading)
+                        .foregroundColor(Color.white)
+                        .shadow(radius: 3)
+                }
+                .padding(.vertical, 0)
+                .padding(.horizontal, 20)
+                .frame(width: 281, height: 105)
+                .background(Color("ColorBlackTransparentLight"))
             }
-            .padding(.vertical, 0)
-            .padding(.horizontal, 20)
-            .frame(width: 281, height: 105)
-            .background(Color("ColorBlackTransparentLight"))
+            .frame(width: 285, height: 105, alignment: .center)
+            .offset(x: -66, y: 75)
         }
         .frame(width: 480, height: 320, alignment: .center)
     }
