4469113257c2d034dfcf37a85dda7b8628e6663d
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-10 13:48:22 -0500

    facts view complete

--- Avocados/View/FactsView.swift
+++ Avocados/View/FactsView.swift
@@ -11,7 +11,9 @@ struct FactsView: View {
     var body: some View {
         ZStack {
             Text("It’s little wonder there are so many health benefits when a single serve of avocado (1/4 or 50g) boasts healthy fats, fibre, folate, niacin, vitamins C, B5, E & K, potassium and antioxidants.")
-                .padding()
+                .padding(.leading, 55)
+                .padding(.trailing, 10)
+                .padding(.vertical, 31)
                 .frame(width: 300, height: 135, alignment: .center)
                 .background(LinearGradient(gradient: Gradient(colors: [Color("ColorGreenMedium"), Color("ColorGreenLight")]), startPoint: .leading, endPoint: .trailing))
                 .cornerRadius(12)
@@ -24,6 +26,21 @@ struct FactsView: View {
                 .resizable()
                 .frame(width: 66, height: 66, alignment: .center)
                 .clipShape(Circle())
+                .background(
+                    Circle()
+                        .fill(Color.white)
+                        .frame(width: 74, height: 74, alignment: .center)
+                )
+                .background(
+                    Circle()
+                        .fill(LinearGradient(colors: [Color("ColorGreenMedium"), Color("ColorGreenLight")], startPoint: .trailing, endPoint: .leading))
+                        .frame(width: 82, height: 82, alignment: .center)
+                )
+                .background(
+                    Circle()
+                        .fill(Color.white)
+                        .frame(width: 90, height: 90, alignment: .center)
+                )
                 .offset(x: -150)
             
         }
