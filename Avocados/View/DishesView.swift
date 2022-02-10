5c7658fd673353a67d04755fbbd35ecac3f94d7d
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-10 09:23:39 -0500

    added left and righ vstaks to dishes view

--- Avocados/View/DishesView.swift
+++ Avocados/View/DishesView.swift
@@ -10,7 +10,76 @@ import SwiftUI
 struct DishesView: View {
     var body: some View {
         HStack () {
+            // Column 1
+            VStack {
+                HStack () {
+                    Image("icon-toasts")
+                        .resizable()
+                        .modifier(IconModifier())
+                    Spacer()
+                    Text("Toasts")
+                }
+                Divider()
+                HStack () {
+                    Image("icon-tacos")
+                        .resizable()
+                        .modifier(IconModifier())
+                    Spacer()
+                    Text("Tacos")
+                }
+                Divider()
+                HStack () {
+                    Image("icon-salads")
+                        .resizable()
+                        .modifier(IconModifier())
+                    Spacer()
+                    Text("Salads")
+                }
+                Divider()
+                HStack () {
+                    Image("icon-halfavo")
+                        .resizable()
+                        .modifier(IconModifier())
+                    Spacer()
+                    Text("Spreads")
+                }
+            }
+            
             Image(systemName: "heart.circle")
+            
+            VStack {
+                HStack () {
+                    Text("Guacamole")
+                    Spacer()
+                    Image("icon-guacamole")
+                        .resizable()
+                        .modifier(IconModifier())
+                }
+                Divider()
+                HStack () {
+                    Text("Sandwiches")
+                    Spacer()
+                    Image("icon-sandwiches")
+                        .resizable()
+                        .modifier(IconModifier())
+                }
+                Divider()
+                HStack () {
+                    Text("Soup")
+                    Spacer()
+                    Image("icon-soup")
+                        .resizable()
+                        .modifier(IconModifier())
+                }
+                Divider()
+                HStack () {
+                    Text("Smoothies")
+                    Spacer()
+                    Image("icon-smoothies")
+                        .resizable()
+                        .modifier(IconModifier())
+                }
+            }
         }
         .font(.system(.callout, design: .serif))
         .foregroundColor(Color.gray)
@@ -19,9 +88,16 @@ struct DishesView: View {
     }
 }
 
+struct IconModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(width: 42, height: 42, alignment: .center)
+    }
+}
+
 struct DishesView_Previews: PreviewProvider {
     static var previews: some View {
         DishesView()
-            .previewLayout(.fixed(width: 414, height: 200))
+            .previewLayout(.fixed(width: 414, height: 240))
     }
 }
