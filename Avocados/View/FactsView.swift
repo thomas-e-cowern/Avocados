12d4dd5c8ff1fc9fc63f3628b4065bd2c733cb30
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-10 13:10:20 -0500

    added text and image to facts view

--- Avocados/View/FactsView.swift
+++ Avocados/View/FactsView.swift
@@ -9,9 +9,24 @@ import SwiftUI
 
 struct FactsView: View {
     var body: some View {
-        Text("It’s little wonder there are so many health benefits when a single serve of avocado (1/4 or 50g) boasts healthy fats, fibre, folate, niacin, vitamins C, B5, E & K, potassium and antioxidants.")
-            .padding()
-            .frame(width: 300, height: 135, alignment: .center)
+        ZStack {
+            Text("It’s little wonder there are so many health benefits when a single serve of avocado (1/4 or 50g) boasts healthy fats, fibre, folate, niacin, vitamins C, B5, E & K, potassium and antioxidants.")
+                .padding()
+                .frame(width: 300, height: 135, alignment: .center)
+                .background(LinearGradient(gradient: Gradient(colors: [Color("ColorGreenMedium"), Color("ColorGreenLight")]), startPoint: .leading, endPoint: .trailing))
+                .cornerRadius(12)
+                .lineLimit(6)
+                .multilineTextAlignment(.leading)
+                .font(.footnote)
+                .foregroundColor(Color.white)
+            
+            Image("avocado-fact-2")
+                .resizable()
+                .frame(width: 66, height: 66, alignment: .center)
+                .clipShape(Circle())
+                .offset(x: -150)
+            
+        }
     }
 }
 
