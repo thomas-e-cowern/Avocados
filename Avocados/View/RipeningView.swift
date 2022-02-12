91d2c61dea82ec9e47d25b815d5e586cba42c9d7
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-12 06:33:13 -0500

    added ripening view

---
+++ Avocados/View/RipeningView.swift
@@ -0,0 +1,20 @@
+//
+//  RipeningView.swift
+//  Avocados
+//
+//  Created by Thomas Cowern New on 2/12/22.
+//
+
+import SwiftUI
+
+struct RipeningView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RipeningView_Previews: PreviewProvider {
+    static var previews: some View {
+        RipeningView()
+    }
+}
